29263ae64edaa3f2f71e49e91809b7bdc2d5f783
gofuckbiz <dadas83@inbox.ru>
2025-01-06 21:16:56 +0300

    Implement HTTP server with /hello and /headers endpoints in main.go

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func hello(w http.ResponseWriter, request *http.Request) {
+
+	fmt.Fprintf(w, "Hello go")
+}
+
+func headers(w http.ResponseWriter, request *http.Request) {
+
+	for name, headers := range request.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
 
 func main() {
 
-	fmt.Println("Hello Go")
-	fmt.Println("Pull request")
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8080", nil)
+
 }
