6d264102310844c83aeea9f6598e02084af9ba46
gofuckbiz <dadas83@inbox.ru>
2025-01-07 13:00:42 +0300

    Add caching functionality with CRUD operations in main.go
    - Implemented a cache using a map to store key-value pairs.
    - Added HTTP handlers for retrieving, setting, and deleting values by key:
      - `ValueByKey`: Retrieves the value for a given key.
      - `SetValue`: Sets a value for a given key.
      - `DeleteValue`: Deletes the value associated with a given key.
    - Updated main function to register new endpoints: /value_key, /set_value, and /delete_key.

--- main.go
+++ main.go
@@ -5,6 +5,53 @@ import (
 	"net/http"
 )
 
+var cache = make(map[string]string)
+
+func ValueByKey(w http.ResponseWriter, request *http.Request) {
+
+	key := request.URL.Query().Get("key")
+
+	value, exists := cache[key]
+
+	if !exists {
+		http.Error(w, "Key not found ", http.StatusNotFound)
+
+	}
+
+	if _, err := fmt.Fprintf(w, "%s", value); err != nil {
+		http.Error(w, "Internal server Error", http.StatusInternalServerError)
+	}
+
+}
+
+func SetValue(w http.ResponseWriter, request *http.Request) {
+
+	key := request.URL.Query().Get("key")
+
+	value := request.URL.Query().Get("value")
+
+	cache[key] = value
+
+	fmt.Fprintf(w, "Value set for key: %s", key)
+
+}
+
+func DeleteValue(w http.ResponseWriter, request *http.Request) {
+
+	// нужно удалить значение по ключу
+
+	key := request.URL.Query().Get("key")
+
+	if _, exists := cache[key]; exists {
+		delete(cache, key)
+		fmt.Fprintf(w, "value deleted for key: %s", key)
+
+	} else {
+		http.Error(w, "key not found", http.StatusNotFound)
+	}
+
+}
+
 func hello(w http.ResponseWriter, request *http.Request) {
 
 	if _, err := fmt.Fprintf(w, "Hello go"); err != nil {
@@ -26,6 +73,9 @@ func main() {
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/value_key", ValueByKey)
+	http.HandleFunc("/set_value", SetValue)
+	http.HandleFunc("/delete_key", DeleteValue)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 
